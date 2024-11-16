db4ed24ca67f198f0f144178443d7add67b12a1b
Z-Haroon <workmailharoon@gmail.com>
2024-11-16 11:11:50 +0500

    Header Completed

--- Villa_Agency/scripts/main.js
+++ Villa_Agency/scripts/main.js
@@ -1,8 +1,24 @@
 (() => {
 
-  window.addEventListener("load", () => {
-    document.querySelector(".loading").classList.add("completed");
-  });
+  const loadingScreenEl = document.querySelector(".loading");
+  const headerEl = document.getElementById("header");
+  const upperHeaderEl = document.querySelector(".upper-header--container");
+  const upperHeaderDividerEl = document.querySelector(".upper-header--divider");
 
+  const loadingCompleted = () => {
+    loadingScreenEl.classList.add("completed");
+  };
 
+  const hasScrolled = () => {
+    let scrollThreshold = 60;
+    let isScrolled = window.scrollY > scrollThreshold;
+
+    headerEl.classList.toggle("scroll", isScrolled);
+    upperHeaderDividerEl.classList.toggle("scroll", isScrolled);
+    upperHeaderEl.classList.toggle("scroll", isScrolled);
+  };
+
+  window.addEventListener("load", loadingCompleted);
+  window.addEventListener("scroll", hasScrolled);
 })();
+

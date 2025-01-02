73813d0b3d0ad8ba27d1adb4eefa5d836e5d6839
Z-Haroon <workmailharoon@gmail.com>
2025-01-02 17:31:27 +0500

    Header Menu Left Hand Side Created Dynamically

--- Facebook/scripts/headerManager.js
+++ Facebook/scripts/headerManager.js
@@ -1,3 +1,56 @@
+// Header Menu Create Post Section links
+const menuCreateData = {
+  listOne: [
+    {imgPath: "./assets/edit.png", name: "Post"},
+    {imgPath: "./assets/book.png", name: "Story"},
+    {imgPath: "./assets/reel.png", name: "Reel"},
+    {imgPath: "./assets/star.png", name: "Life Event"},
+  ],
+  listSecond: [
+    {imgPath: "./assets/flag.png", name: "Page"},
+    {imgPath: "./assets/megaphone.png", name: "Ad"},
+    {imgPath: "./assets/menu-group.png", name: "Group"},
+    {imgPath: "./assets/event.png", name: "Event"},
+    {imgPath: "./assets/store.png", name: "Marketplace Listing"},
+  ]
+}
+
+// Render Header Menu Post Section Elements
+const renderPostSectionElements = (loopName, appendContainer) => {
+  // Error Handling
+  if(!loopName && !appendContainer) return;
+
+  // References
+  let showElementsContainerEl = document.querySelector(appendContainer);
+  let elementTemplate = document.getElementById("header-menu--item");
+
+  // For Each For Render
+  loopName.forEach(currentElement => {
+    // Import Elements From Template
+    let element = document.importNode(elementTemplate.content, true);
+    // Change Element Details According to array Data
+    element.querySelector(".img").src = currentElement.imgPath;
+    element.querySelector(".img").alt = currentElement.name;
+    element.querySelector(".name").textContent = currentElement.name;
+    // Show Element
+    showElementsContainerEl.appendChild(element);
+  });
+};
+
+
+
+// Hide Menues On Window Click
+const hideMenuesOnWindowClick = (commonClass, hideThisElement) => {
+  // Error Handling
+  if(!commonClass || !hideThisElement) return ;
+
+  window.addEventListener("click", (e) => {
+    // If Target Element Not Have CommonClass It's Hide Automatically
+    if(!e.target.classList.contains(commonClass)) {
+      hideThisElement.classList.remove("active");
+    }
+  })
+}
 export const headerManager = () => {
   // Get all header icons
   let headerIconEl = document.querySelectorAll(".js-header--icon");
@@ -24,4 +77,26 @@ export const headerManager = () => {
       popupMessageEl.classList.remove("show");
     });
   });
+
+  // Show Header Menu OnClick
+  let openMenuOnClick = (element, openThisContainer, commonClass) => {
+
+    // Reference
+    let targetElement = document.querySelector(element);
+    let showThisContainerEl = document.querySelector(openThisContainer);
+    
+    // Error Handling
+    if(!targetElement || !showThisContainerEl || !commonClass) return;
+    // Event Listner
+    targetElement.addEventListener("click", (e) => {
+        showThisContainerEl.classList.toggle("active");  
+    });
+    hideMenuesOnWindowClick(commonClass, showThisContainerEl);
+  };
+
+  openMenuOnClick(".header-menu", ".header-menu--section", "h-menu")
+  // Calling Function For Render Elements On Header Menu
+  renderPostSectionElements(menuCreateData.listOne, ".menu-items_1--container");
+  // Second Row
+  renderPostSectionElements(menuCreateData.listSecond, ".menu-items_2--container");
 };
--- Facebook/scripts/main.js
+++ Facebook/scripts/main.js
@@ -5,6 +5,12 @@ const loadingComplete = () => {
   // Referece Of Loading Screen Element
   let loadingScreenElement = document.querySelector(".loading");
 
+  // Error Handling
+  if(!loadingScreenElement) {
+    console.log("Something went Wrong Please check loading Screen Element");
+    return;
+  }
+
   // Adding Class For Hide Loading Screen
   loadingScreenElement.classList.add("active")
 }
